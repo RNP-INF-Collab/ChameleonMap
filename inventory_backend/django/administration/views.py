566e6b1640a3c34b8cad39d3b6b820606e9a5d23
Gabrielvassoler <gabiv98@gmail.com>
2024-09-23 09:51:29 -0300

    Update: Nginx cyphers, ssl, and gets on backend

--- inventory_backend/django/administration/views.py
+++ inventory_backend/django/administration/views.py
@@ -1,33 +1,34 @@
-from django.shortcuts import render
-from rest_framework import viewsets
 from .models import *
 from .serializers import *
+from rest_framework import viewsets
 
-class MenuViewSet(viewsets.ModelViewSet):
+class ReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
+    http_method_names = ['get']
+
+class MenuViewSet(ReadOnlyViewSet):
     serializer_class = MenuSerializer
     queryset = Menu.objects.all()
 
-class TagViewSet(viewsets.ModelViewSet):
+class TagViewSet(ReadOnlyViewSet):
     serializer_class = TagSerializer
     queryset = Tag.objects.all()
 
-class LocationViewSet(viewsets.ModelViewSet):
+class LocationViewSet(ReadOnlyViewSet):
     serializer_class = LocationSerializer
     queryset = Location.objects.all()
 
-class LinkViewSet(viewsets.ModelViewSet):
+class LinkViewSet(ReadOnlyViewSet):
     serializer_class = LinkSerializer
     queryset = Link.objects.all()
 
-class Links_groupViewSet(viewsets.ModelViewSet):
-
+class Links_groupViewSet(ReadOnlyViewSet):
     serializer_class = Links_groupSerializer
     queryset = Links_group.objects.all()
 
-class Tag_relationshipViewSet(viewsets.ModelViewSet):
+class Tag_relationshipViewSet(ReadOnlyViewSet):
     serializer_class = Tag_relationshipSerializer
     queryset = Tag_relationship.objects.all()
 
-class Map_configurationViewSet(viewsets.ModelViewSet):
+class Map_configurationViewSet(ReadOnlyViewSet):
     serializer_class = Map_configurationSerializer
     queryset = Map_configuration.objects.all()
