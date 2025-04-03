da452d3754482e7a7674965eacaa98e7ed69b0fb
eduardoperetto <erperetto@gmail.com>
2025-04-03 09:05:21 -0300

    fix: onboarding

--- map-frontend/src/app/tooltip/tooltip.component.ts
+++ map-frontend/src/app/tooltip/tooltip.component.ts
@@ -35,7 +35,8 @@ export class TooltipComponent {
 
   public showOnboardingIfNeeded(mapName: String) {
     // Check if the user has visited before using localStorage
-    if (localStorage.getItem('firstVisit') == null && window.innerWidth > 500) {
+    const shouldShowOnboarding = localStorage.getItem('firstVisit') == null && window.innerWidth > 500;
+    if (shouldShowOnboarding) {
       this.askToStartOnboarding(mapName);
       //localStorage.setItem('firstVisit', 'no');
     }
@@ -62,59 +63,65 @@ export class TooltipComponent {
   }
 
   continueOnboarding() {
-    this.map.dragging.disable();
     /* Menu tabs */
     if (!this.hasShownTabsTooltip) {
-      console.log(2);
-      let tabs = document.getElementById('menu-groups-tabs') as HTMLDivElement;
-      if (tabs != null) {
-        this.quoteCentered = false;
-        this.addTooltipNextTo(
-          tabs,
-          'At the top right, you will find <b>Menu Groups</b>. They are tabs that contain different visualization contexts. <br>Select a tab, and different menus will be shown below.</br>',
-          0
-        );
-        this.hasShownTabsTooltip = true;
+      this.hasShownTabsTooltip = true;
+      let menuGroups = document.getElementById('menu-groups-tabs') as HTMLDivElement;
+      if (!menuGroups) {
+        this.continueOnboarding();
         return;
       }
-      this.hasShownTabsTooltip = true;
-      this.continueOnboarding();
+      this.quoteCentered = false;
+      this.addTooltipNextTo(
+        menuGroups,
+        'At the top right, you will find <b>Menu Groups</b>. They are tabs that contain different visualization contexts. <br>Select a tab, and different menus will be shown below.</br>',
+        0
+      );
       return;
     }
     /* Menus */
     if (!this.hasShownMenusTooltip) {
-      let menus = document.getElementById('menu-button-expand') as HTMLDivElement;
+      this.hasShownMenusTooltip = true;
+      let menus = document.getElementById('menu-button') as HTMLDivElement;
+      if (!menus) {
+        this.continueOnboarding();
+        return;
+      }
       this.quoteCentered = false;
       this.addTooltipNextTo(
         menus,
         'Each rectangular panel is a <b>Menu</b>. They group related items (tags). <br>Menus can be selected, enabling the visualization of the items that are related to that menu.</br>'
       );
-      this.hasShownMenusTooltip = true;
       return;
     }
     /* Tags */
     if (!this.hasShownTagsTooltip) {
-      this.quoteCentered = false;
+      this.hasShownTagsTooltip = true;
       let tags = document.getElementById('eye') as HTMLDivElement;
+      if (!tags) {
+        this.continueOnboarding();
+        return;
+      }
+      this.quoteCentered = false;
+      this.map.dragging.disable();
       this.addTooltipNextTo(
         tags,
         'Each menu contains geolocated items, known as <b>Tags</b>, each identified by name and color.<br> Each tag is related to one or more tags in the map. You can identify them in the map by their color. You can also hide a tag clicking on the eye icon on their left. </br>'
       );
-      this.hasShownTagsTooltip = true;
       return;
     }
     /* Location */
     if (!this.hasShownLocationTooltip) {
+      this.hasShownLocationTooltip = true;
       let location = this.getLocationOnMap();
       if (location) {
         this.selectedLocation = location;
         this.addTooltipNextTo(
           location,
-          'In the map, there are geolocated points, known as <b>Locations</b>.<br>Through their colors you will be able to see which <i>tags</i> they relate to.<br> Clicking a location opens up a <b>popup</b> with further details.<br> That is it! Thanks for your visit!</b>'
+          'In the map, there are geolocated points, known as <b>Locations</b>.<br>Through their colors you will be able to see which <i>tags</i> they relate to.<br> Clicking a location opens up a <b>popup</b> with further details.<br> Thanks for your visit!</b>'
         );
         this.isLastTooltip = true;
         this.continueText = "End tutorial";
-        this.hasShownLocationTooltip = true;
       }
       return;
     }
@@ -154,24 +161,24 @@ export class TooltipComponent {
   }
 
   click(x: number, y: number) {
-      const ev = new MouseEvent('click', {
-          'view': window,
-          'bubbles': true,
-          'cancelable': true,
-          'screenX': x,
-          'screenY': y
-      });
+    const ev = new MouseEvent('click', {
+      'view': window,
+      'bubbles': true,
+      'cancelable': true,
+      'screenX': x,
+      'screenY': y
+    });
 
-      const el = document.elementFromPoint(x, y);
-      if (!el) { return }
-      el.dispatchEvent(ev);
+    const el = document.elementFromPoint(x, y);
+    if (!el) { return }
+    el.dispatchEvent(ev);
   }
 
 
-  addTooltipNextTo(div: HTMLDivElement|null, text: string, top: number|null = null, onRight = true) {
+  addTooltipNextTo(div: HTMLDivElement | null, text: string, top: number | null = null, onRight = true) {
     if (!div) { return }
 
-    const aproxHeight = 75 + Math.ceil(text.length/38)*19;
+    const aproxHeight = 75 + Math.ceil(text.length / 38) * 19;
 
     const stepTooltip = document.getElementsByClassName('step-tooltip')[0] as HTMLDivElement;
     const textDiv = stepTooltip.getElementsByClassName('tooltip-text')[0];
@@ -181,19 +188,19 @@ export class TooltipComponent {
     let left = div.getBoundingClientRect().left + 15;
     if (onRight) {
       left += div.offsetWidth;
-    }  else {
-      left += div.offsetWidth/2;
+    } else {
+      left += div.offsetWidth / 2;
     }
     stepTooltip.style.left = `${left}px`;
     if (top == null) {
-      const quoteTop = this.quoteCentered ? aproxHeight/2 : aproxHeight/8;
-      top = div.getBoundingClientRect().top + div.offsetHeight/2 - quoteTop;
+      const quoteTop = this.quoteCentered ? aproxHeight / 2 : aproxHeight / 8;
+      top = div.getBoundingClientRect().top + div.offsetHeight / 2 - quoteTop;
     }
     stepTooltip.style.top = `${top}px`;
     this.currentStepTooltipEnabled = true;
   }
 
-  private getLocationOnMap(): HTMLDivElement|undefined {
+  private getLocationOnMap(): HTMLDivElement | undefined {
     const locations = document.getElementsByClassName('custom-pin');
     for (let i = 0; i < locations.length; i++) {
       const pos = locations[i].getBoundingClientRect();
@@ -204,10 +211,10 @@ export class TooltipComponent {
     return;
   }
 
-  private getOpenOverlayedPopupIcon(): HTMLDivElement|undefined {
+  private getOpenOverlayedPopupIcon(): HTMLDivElement | undefined {
     let popupIcon = document.getElementsByClassName('opp-expand-icon opp-open-button !hidden');
-    if (!popupIcon || !popupIcon.length) { 
-      return; 
+    if (!popupIcon || !popupIcon.length) {
+      return;
     }
     return popupIcon[0] as HTMLDivElement;;
   }
