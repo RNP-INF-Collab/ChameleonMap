5b17b60c653872370a6537efd8fa33fe8c991381
eduardoperetto <erperetto@gmail.com>
2025-05-05 17:00:24 -0300

    feat: add staff privilege by default

--- inventory_backend/django/clients/admin.py
+++ inventory_backend/django/clients/admin.py
@@ -4,7 +4,9 @@ from tenant_users.tenants.tasks import provision_tenant
 from clients.models import Client, Domain, TenantUser
 from django.contrib import admin
 from django.core.exceptions import ObjectDoesNotExist
-    
+from django_tenants.utils import tenant_context
+from tenant_users.permissions.models import UserTenantPermissions
+
 @admin.register(Client)
 class ClientAdmin(TenantAdminMixin, admin.ModelAdmin):
     list_display = ('name','owner',)
@@ -26,6 +28,28 @@ class DomainAdmin(TenantAdminMixin, admin.ModelAdmin):
         return False
 
 @admin.register(TenantUser)
-class DomainAdmin(TenantAdminMixin, admin.ModelAdmin):
+class TenantUserAdmin(TenantAdminMixin, admin.ModelAdmin):
     list_display = ('email','name',)
     exclude = ('password',)
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        grant_privileges(obj)
+
+# Tenant-users only allow logins in the admin from staff users
+# This function add this permission by default to all related tenants
+def grant_privileges(user_obj):
+    tenants = list(user_obj.tenants.all())
+    user_obj.tenants.clear()
+    for tenant in tenants:
+            with tenant_context(tenant):
+                UserTenantPermissions.objects.create(
+                    profile=user_obj,
+                    is_staff=True,
+                    is_superuser=True,
+                )
+                user_obj.tenants.add(tenant)
+                user_obj.save()
+
+@admin.register(UserTenantPermissions)
+class UserTenantPermissionsAdmin(TenantAdminMixin, admin.ModelAdmin):
+    pass
