b0aff595933ced75972396d74acce3ddde96b364
eduardoperetto <erperetto@gmail.com>
2025-05-16 18:09:00 -0300

    remove SOS RS reference

--- map-frontend/src/app/overlayed-popup/overlayed-popup.component.ts
+++ map-frontend/src/app/overlayed-popup/overlayed-popup.component.ts
@@ -113,7 +113,7 @@ export class OverlayedPopupComponent {
     }
 
     // Set title
-    this.overlayedPopupTitle.nativeElement.innerHTML = `<div>${this.getPopupTitle()} <span style="font-size: smaller; font-weight: normal;">(Dados obtidos de: <a href="https://sos-rs.com/">SOS-RS</a>)</span></div>`;
+    this.overlayedPopupTitle.nativeElement.innerHTML = `<div>${this.getPopupTitle()}</div>`;
 
     // Set Content
     this.subMap.keeper = this._currentKeeper;
