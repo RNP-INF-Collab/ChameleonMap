f6e10131c7b84d7eba93b875608184eb7738eda6
leonardolauryel <leonardolauryel@hotmail.com>
2025-06-04 00:12:01 -0300

    feat: Adding custom email content

--- inventory_backend/django/clients/urls.py
+++ inventory_backend/django/clients/urls.py
@@ -43,7 +43,14 @@ urlpatterns = [
     path('', include(router.urls)),
     path('import/', include("importer.urls")),
     path('atlas/', include("atlas_builder.urls")),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(
+            subject_template_name='registration/password_reset_subject_en.txt',
+            html_email_template_name='registration/password_reset_email_en.html'
+        ),
+        name='password_reset'
+    ),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
--- inventory_backend/django/inventory_backend/urls.py
+++ inventory_backend/django/inventory_backend/urls.py
@@ -10,7 +10,14 @@ admin.site.site_header = 'Map Administration'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(
+            subject_template_name='registration/password_reset_subject_en.txt',
+            html_email_template_name='registration/password_reset_email_en.html'
+        ),
+        name='password_reset'
+    ),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
